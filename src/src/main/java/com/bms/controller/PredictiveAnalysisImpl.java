038205d62c9f4bc0d3a1e229fcaf4012cac2d9b9
Colin <34100863+colincurran123@users.noreply.github.com>
2017-11-30 03:11:31 +0000

    updated in line with the update to its interface

--- src/src/main/java/com/bms/controller/PredictiveAnalysisImpl.java
+++ src/src/main/java/com/bms/controller/PredictiveAnalysisImpl.java
@@ -16,7 +16,6 @@ public class PredictiveAnalysisImpl implements PredictiveAnalysis {
 	}
 
 	
-	@Override
 	public float fuelEffCal() {
 		// TODO Auto-generated method stub
 		distance = database.getDistance();
